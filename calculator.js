f62542837f5f2b3ab6fa36d23bb6ad129fd250de
pavithraravi1902 <pavithraravi1902@gmail.com>
2024-10-18 20:29:35 +0530

    custom delimiter implemented using regexp

--- calculator.js
+++ calculator.js
@@ -3,8 +3,13 @@ function add(numbers) {
     if (numbers === "") {
         return 0;
     }
-
-    const numArray = numbers.split(",");
+    let delimiter = /,|\n/;
+    if (numbers.startsWith("//")) {
+        const parts = numbers.split("\n", 2);
+        delimiter = new RegExp(parts[0].substring(2));
+        numbers = parts[1];
+    }
+    const numArray = numbers.split(delimiter);
     return numArray.reduce((sum, num) => sum + parseInt(num, 10), 0);
 }
 
