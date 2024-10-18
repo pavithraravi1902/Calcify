1dd36b0627106a974315f99542a57c8d4c3e62c2
pavithraravi1902 <pavithraravi1902@gmail.com>
2024-10-18 20:25:52 +0530

    Updated add function to handle multiple comma-separated numbers

--- calculator.js
+++ calculator.js
@@ -1,14 +1,13 @@
+
 function add(numbers) {
     if (numbers === "") {
         return 0;
     }
 
     const numArray = numbers.split(",");
-    if (numArray.length === 1) {
-        return parseInt(numArray[0], 10);
-    }
-
-    return parseInt(numArray[0], 10) + parseInt(numArray[1], 10);
+    return numArray.reduce((sum, num) => sum + parseInt(num, 10), 0);
 }
 
+
 export default add;
+
