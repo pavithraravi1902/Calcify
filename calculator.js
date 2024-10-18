ed20abbb3a8618dc8c201d4c4d5766dbd96150b9
pavithraravi1902 <pavithraravi1902@gmail.com>
2024-10-18 19:03:07 +0530

    Implemented basic add function with empty string

--- calculator.js
+++ calculator.js
@@ -1,1 +1,14 @@
-// Calculation logic
+function add(numbers) {
+    if (numbers === "") {
+        return 0;
+    }
+
+    const numArray = numbers.split(",");
+    if (numArray.length === 1) {
+        return parseInt(numArray[0], 10);
+    }
+
+    return parseInt(numArray[0], 10) + parseInt(numArray[1], 10);
+}
+
+export default add;
